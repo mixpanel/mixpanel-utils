831e2bde3ea4190e6667771d7351bacea3f78af2
jaredmixpanel <jared@mixpanel.com>
2020-09-28 11:39:04 -0700

    add setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+# from distutils.core import setup
+from setuptools import setup
+
+setup(
+    name='mixpanel_api',
+    packages=['mixpanel_api'],
+    version='1.6.3',
+    description='Utilities-only module for exporting and importing data into Mixpanel. NOT for server-side app tracking.',
+    long_description='A utilities-only module for exporting and importing data into Mixpanel via their APIs. This is NOT for server-side app tracking.',
+    author='Jared McFarland',
+    author_email='jared@mixpanel.com',
+    url='http://www.mixpanel.com'
+
+)
