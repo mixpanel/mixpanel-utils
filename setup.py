3cdbe87699b8331c6dd9ec54b93ad2e2940d5f66
Kaodiak Cupcake <kao.dev.moo@gmail.com>
2025-03-28 16:52:16 -0500

    minor version bump with data residency as param

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup
 
 setup(
-    name='mixpanel-utils',
-    packages=['mixpanel_utils'],
-    package_dir={'':'src'},
-    version='2.2.6',
-    description='Utilities-only module for exporting and importing data into Mixpanel. NOT for server-side app tracking.',
-    long_description='A utilities-only module for exporting and importing data into Mixpanel via their APIs. This is NOT for server-side app tracking.',
-    author='Jared McFarland',
-    author_email='jared@mixpanel.com',
-    url='https://github.com/mixpanel/mixpanel-utils',
-    python_requires='>=3'
+    name="mixpanel-utils",
+    packages=["mixpanel_utils"],
+    package_dir={"": "src"},
+    version="2.2.7",
+    description="Utilities-only module for exporting and importing data into Mixpanel. NOT for server-side app tracking.",
+    long_description="A utilities-only module for exporting and importing data into Mixpanel via their APIs. This is NOT for server-side app tracking.",
+    author="Jared McFarland",
+    author_email="jared@mixpanel.com",
+    url="https://github.com/mixpanel/mixpanel-utils",
+    python_requires=">=3",
 )
